d071e2b2b457cd9d8379333f2671be98d452b17b
Narmondil <a.vigneron@protonmail.com>
2020-03-11 20:46:55 +0100

    Collatz conjecture in Haskell

---
+++ haskell/CollatzConjecture.hs
@@ -0,0 +1,17 @@
+module CollatzConjecture (collatz) where
+
+collatz :: Integer -> Maybe Integer
+collatz n
+        | n <= 0    = Nothing
+        | otherwise = Just $ collatzAux 0 n
+
+
+collatzAux :: Integer -> Integer -> Integer
+collatzAux counter n 
+        | n == 1        = counter
+        | r == 0        = collatzAux newCounter q
+        | otherwise     = collatzAux  newCounter $ n *3 +1
+    where
+        newCounter = counter +1
+        (q,r)      = n `quotRem` 2
+        
